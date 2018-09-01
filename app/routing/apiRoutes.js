2f0c5dfb9852cbe5cf203d5bb628cd2788b656cd
thmiv <thmiv4@gmail.com>
2018-08-31 19:33:32 -0500

    functioning friend finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
 const peopleData = require("../data/friends");
@@ -11,30 +9,16 @@ const peopleData = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(peopleData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-      peopleData.push(req.body);
-      console.log(peopleData[peopleData.length - 1])
+    var yourMatch = getMatch(req.body);
+    peopleData.push(req.body);
+    //console.log(yourMatch);
+    res.json(yourMatch);
   });
 }
 
@@ -44,54 +28,43 @@ module.exports = function(app) {
 
 function getMatch(userData) {
   var scoreDifferences = [];
-  var userScores =  req.body.scores;
-  console.log('\n' + userScores + '\n');
+  var userScores = userData.scores;
 
-  peopleData.forEach(function(person, pIndex) {
+  peopleData.forEach(function (person, pIndex) {
       var personDifference = 0;
       var personScores = person.scores;
 
       for (i = 0; i < personScores.length; i++) {
           var questionDifference = parseInt(personScores[i]) - parseInt(userScores[i]);
 
-          personDifference += Math.abs(questionDifference); 
+          personDifference += Math.abs(questionDifference);
       }
 
       var personObject = {
-        id: pIndex,
-        score: personDifference
+          id: pIndex,
+          score: personDifference
       }
 
       scoreDifferences.push(personObject);
   });
   var sortedScores = bubbleSort(scoreDifferences);
-  console.log(sortedScores);
-  console.log("\nbest match is " + peopleData[sortedScores[0].id].name);
-  console.log(" ");
+  return peopleData[sortedScores[0].id];
 }
 
 function bubbleSort(scoreArray) {
-  // sorted acts as a flag to let us know if our array has been completely sorted
   var sorted = false;
 
   while (!sorted) {
-    sorted = true;
-    // Loop through the array
-    for (var i = 0; i < scoreArray.length - 1; i++) {
-      // If the current element is larger than the next element, swap them and set sorted to `false`
-      if (scoreArray[i].score > scoreArray[i + 1].score) {
-        sorted = false;
-        var temp = scoreArray[i];
-        scoreArray[i] = scoreArray[i + 1];
-        scoreArray[i + 1] = temp;
+      sorted = true;
+      for (var i = 0; i < scoreArray.length - 1; i++) {
+          if (scoreArray[i].score > scoreArray[i + 1].score) {
+              sorted = false;
+              var temp = scoreArray[i];
+              scoreArray[i] = scoreArray[i + 1];
+              scoreArray[i + 1] = temp;
+          }
       }
-    }
   }
 
-  // If we looped through the array without having to swap anything, exit the while loop and return the array
   return scoreArray;
-
-  // scoreArray.forEach(function(item) {
-  //   console.log("item score: " + item.score);
-  // })
 }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Reference code [https://github.com/the-Coding-Boot-Camp-at-UT/UTAUS201807FSF2-FT/tree/master/class-content/13-express/01-Activities/16-HotRestaurant/Solved]
 // ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
 const express = require("express");
@@ -9,23 +8,17 @@ const bodyParser = require("body-parser");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 // ==============================================================================
 
-// Tells node that we are creating an "express" server
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
 require("./app/routing/apiRoutes")(app);
@@ -33,7 +26,6 @@ require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
-// The below code effectively "starts" our server
 // =============================================================================
 
 app.listen(PORT, function() {
